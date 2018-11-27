f204ed50b9edc25371e8b1b48be714167e53050e
Dennis Xiao <dennisboys@gmail.com>
2018-11-27 20:30:42 +1000

    add register UI

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,11 +1,74 @@
 import React, { Component } from 'react'
+import { Grid, Form, Segment, Button, Header, Message, Icon } from "semantic-ui-react"
+import { Link } from 'react-router-dom'
 
 class Register extends Component {
+  handleChange = () => {
+  }
+
   render () {
     return (
-      <div>
-        Register
-      </div>
+      <Grid
+        textAlign="center"
+        verticalAlign="middle"
+        padded>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as='h2' icon color='orange' textAlign='center'>
+            <Icon name='puzzle piece' color='orange'/>
+            Register for DevChat
+          </Header>
+
+          <Form size='large'>
+            <Segment stacked>
+              {/* username filed */}
+              <Form.Input
+                fluid
+                type='text'
+                name='username'
+                icon='user'
+                iconPosition='left'
+                placeholder='Username'
+                onChange={this.handleChange}/>
+
+              {/* email field */}
+              <Form.Input
+                fluid
+                type='text'
+                name='email'
+                icon='mail'
+                iconPosition='left'
+                placeholder='Email Address'
+                onChange={this.handleChange}/>
+
+              {/* password field */}
+              <Form.Input
+                fluid
+                type='password'
+                name='password'
+                icon='lock'
+                iconPosition='left'
+                placeholder='Password'
+                onChange={this.handleChange}/>
+
+              {/* password confirmation field */}
+              <Form.Input
+                fluid
+                type='password'
+                name='passwordConfirmation'
+                icon='repeat'
+                iconPosition='left'
+                placeholder='Password Confirmation'
+                onChange={this.handleChange}/>
+
+              <Button color='orange' fluid size='large'>
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+
+          <Message>Already a user? <Link to='/login'>Login</Link></Message>
+        </Grid.Column>
+      </Grid>
     )
   }
 }
