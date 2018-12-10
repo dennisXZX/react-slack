f9a9d1f42fb85dbb57dc21fd1ec30a00c547d7de
Dennis Xiao <dennisboys@gmail.com>
2018-12-10 21:55:41 +1000

    add messages component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import MessagesHeader from './MessagesHeader'
+import MessageForm from './MessageForm'
+import { Segment, Comment } from 'semantic-ui-react'
 
 class Messages extends Component {
   render () {
     return (
-      <div>messages</div>
+      <Fragment>
+        <MessagesHeader />
+
+        <Segment>
+          <Comment.Group className="messages">
+            {/* Messages */}
+          </Comment.Group>
+        </Segment>
+
+        <MessageForm />
+      </Fragment>
     )
   }
 }
