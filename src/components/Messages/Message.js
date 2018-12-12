ca166f493d0174a755991a1c83e62e428931886b
Dennis Xiao <dennisboys@gmail.com>
2018-12-12 20:39:08 +1000

    add message component

---
+++ src/components/Messages/Message.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import moment from 'moment'
+import { Comment } from 'semantic-ui-react'
+
+const isOwnMessage = (message, user) => {
+  return message.user.id === user.uid ? 'message__self' : ''
+}
+
+const timeFromNow = timestamp => moment(timestamp).fromNow()
+
+class Message extends Component {
+  render () {
+    const { message, user } = this.props
+
+    return (
+      <Comment>
+        <Comment.Avatar src={message.user.avatar} />
+        <Comment.Content className={isOwnMessage(message, user)}>
+          <Comment.Author as="a">
+            {message.user.name}
+          </Comment.Author>
+
+          <Comment.Metadata>
+            {timeFromNow(message.timestamp)}
+          </Comment.Metadata>
+
+          <Comment.Text>
+            {message.content}
+          </Comment.Text>
+        </Comment.Content>
+      </Comment>
+    )
+  }
+}
+
+export default Message
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,16 +1,54 @@
 import React, { Component, Fragment } from 'react'
 import MessagesHeader from './MessagesHeader'
 import MessageForm from './MessageForm'
+import Message from './Message'
 import { Segment, Comment } from 'semantic-ui-react'
 import firebase from '../../firebase'
 
 class Messages extends Component {
   state = {
-    messagesRef: firebase.database().ref('messages')
+    messagesRef: firebase.database().ref('messages'),
+    messages: [],
+    messagesLoading: true
   }
 
+  componentDidMount () {
+    const { currentChannel, currentUser } = this.props
+
+    if (currentChannel && currentUser) {
+      this.addListeners(currentChannel.id)
+    }
+  }
+
+  addListeners = channelId => {
+    this.addMessageListener(channelId)
+  }
+
+  addMessageListener = channelId => {
+    let loadedMessages = []
+
+    this.state.messagesRef.child(channelId).on('child_added', snap => {
+      loadedMessages.push(snap.val())
+
+      this.setState({
+      	messages: loadedMessages,
+        messagesLoading: false
+      })
+    })
+  }
+
+  displayMessages = messages => (
+     messages.length > 0 && messages.map(message => (
+        <Message
+          key={message.timestamp}
+          message={message}
+          user={this.props.currentUser}
+        />
+    ))
+  )
+
   render () {
-    const { messagesRef } = this.state
+    const { messagesRef, messages } = this.state
     const { currentChannel, currentUser } = this.props
 
     return (
@@ -19,7 +57,7 @@ class Messages extends Component {
 
         <Segment>
           <Comment.Group className="messages">
-            {/* Display Messages */}
+            {this.displayMessages(messages)}
           </Comment.Group>
         </Segment>
 
