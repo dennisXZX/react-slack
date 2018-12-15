684c7fe3c57b86def1944dc4c228fc4407fa072d
Dennis Xiao <dennisboys@gmail.com>
2018-12-15 19:50:17 +1000

    show user name that direct message can be sent

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -19,7 +19,7 @@ class SidePanel extends Component {
       >
         <UserPanel currentUser={currentUser} />
         <Channels currentUser={currentUser} />
-        <DirectMessage />
+        <DirectMessage currentUser={currentUser} />
       </Menu>
     )
   }
